95517a4e1e73f1fb110da4429ed3083c5ca6475b
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-02-18 09:55:00 +0800

    Links dropdown

--- src/components/header/LinksDropdown.tsx
+++ src/components/header/LinksDropdown.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import { LucideTextAlignJustify } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+import { links } from "./utils/data";
+
 export default function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <LucideTextAlignJustify />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        {links.map((link) => (
+          <DropdownMenuItem asChild key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
---
+++ src/components/header/utils/data.ts
@@ -0,0 +1,10 @@
+import { Link } from "@/components/header/utils/types";
+
+export const links: Link[] = [
+  { href: "/", label: "Home" },
+  { href: "/products", label: "Products" },
+  { href: "/favorites", label: "Favorites" },
+  { href: "/reviews", label: "Reviews" },
+  { href: "/cart", label: "Cart" },
+  { href: "/orders", label: "Orders" },
+];
