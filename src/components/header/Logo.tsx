61a8fc2126df35feab0a8bd9318041da1e045dde
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-02-18 09:12:32 +0800

    Logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/images/logo.png";
+
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <Image
+        src={logoImg}
+        alt="logo"
+        width={32}
+        height={32}
+        className="size-8"
+      />
+
+      <h1 className="text-xl font-extrabold tracking-tight">Next Store</h1>
+    </Link>
+  );
 }
