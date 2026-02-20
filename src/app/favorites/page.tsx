b6c139547fcd55e3b15a5daf25a3095564420dfc
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-02-20 13:02:03 +0800

    Favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from "next";
 
+import Section from "@/components/Section";
+import ProductGrid from "@/features/product/components/ProductGrid";
+
+import { getFavorites } from "@/features/favorite/utils/actions";
+
 export const metadata: Metadata = {
   title: "Favorites",
 };
 
-export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+export default async function FavoritesPage() {
+  const favorites = await getFavorites();
+
+  return (
+    <Section title="Favorites">
+      <ProductGrid products={favorites.map((favorite) => favorite.product)} />
+    </Section>
+  );
 }
